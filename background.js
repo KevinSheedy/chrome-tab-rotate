b93c3c88dbd682202e4caf990d73d559465e258f
Kevin Sheedy <kevin.x.sheedy@aib.ie>
2015-09-15 13:12:39 +0100

    Default settings

--- background.js
+++ background.js
@@ -30,17 +30,39 @@ function save() {
 	gSettingsUrl = jQuery("#settingsUrl").val();
 	chrome.storage.sync.set({"settingsUrl": gSettingsUrl});
 
+	gConfig = JSON.parse(jQuery("#settingsTextArea").val());
+	chrome.storage.sync.set({"settings": gConfig});
+
+
 }
 
 function readUrlFromStorage() {
 
-	chrome.storage.sync.get("settingsUrl", function(data) {
-		if(data.settingsUrl) {
-			gSettingsUrl = data.settingsUrl;
+	chrome.storage.sync.get("settingsUrl", function(storage) {
+		if(storage.settingsUrl) {
+			gSettingsUrl = storage.settingsUrl;
 		}
 		else {
 			gSettingsUrl = "config.sample.js";
 			chrome.storage.sync.set({"settingsUrl": gSettingsUrl});
+		}
+		jQuery("#settingsUrl").val(gSettingsUrl);
+	});
+}
+
+function readSettingsFromStorage() {
+
+	chrome.storage.sync.get("settings", function(storage) {
+		if(storage.settings) {
+			jQuery("#settingsTextArea").val(storage.settings);
+			gConfig = JSON.parse(storage.settings);
+		}
+		else {
+			jQuery.get("config.sample.js", null, function(response) {
+				jQuery("#settingsTextArea").val(response);
+				gConfig = JSON.parse(response);
+				chrome.storage.sync.set({"settings": response});
+			});
 		}
 		jQuery("#settingsUrl").val(gSettingsUrl);
 	});
@@ -52,8 +74,30 @@ function initEventHandlers() {
 	jQuery("#start").click(start);
 	jQuery("#stop").click(stop);
 	jQuery("#save").click(save);
+	jQuery("#fetchSettings").click(fetchSettings);
 
+	//chrome.storage.sync.clear();
 	readUrlFromStorage();
+	readSettingsFromStorage();
+}
+
+function fetchSettings() {
+	$.ajax({
+		dataType: "text",
+		url: gSettingsUrl,
+		success: fetchSuccess,
+		error: fetchError
+	});
+}
+
+function fetchSuccess(response) {
+	//jQuery("#settingsTextArea").val(JSON.stringify(response, null, 4));
+	jQuery("#settingsTextArea").val(response);
+
+}
+
+function fetchError() {
+	console.log("fetch error");
 }
 
 function loadSettings() {
@@ -86,7 +130,7 @@ function getTabsToClose() {
 function closeTabs(tabIds) {
 
 	chrome.tabs.remove(tabIds, function() {
-		loadConfig();
+		getCurrentTab();
 	})
 }
 
