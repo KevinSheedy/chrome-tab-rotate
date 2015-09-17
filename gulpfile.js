3e8048ae74e29d53b70fd44e5e0b93c4806abea6
Kevin Sheedy <kevin.x.sheedy@aib.ie>
2015-09-17 17:39:53 +0100

    Create zip using gulp - increment version number

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var del = require('del');
+var zip = require('gulp-zip');
+var manifest = require('./manifest.json');
 var mainBowerFiles = require('main-bower-files');
 
 gulp.task('app', function() {
@@ -11,13 +13,13 @@ gulp.task('project', function() {
 });
 
 gulp.task('bower', function() {
-  return gulp.src([
-  		  '**/jquery.min.js'
-  		, '**/angular.min.js'
-  		, '**/bootstrap.min.css'
+	return gulp.src([
+		  '**/jquery.min.js'
+		, '**/angular.min.js'
+		, '**/bootstrap.min.css'
 
-  	])
-  .pipe(gulp.dest('dest'));
+	])
+	.pipe(gulp.dest('dest'));
 });
 
 // gulp.task('bower_foo', function() {
@@ -29,4 +31,10 @@ gulp.task('clean', function() {
 	return del(['dest']);
 });
 
-gulp.task('default', ['clean', 'project', 'app', 'bower']);
+gulp.task('zip', function () {
+	return gulp.src('dest/**')
+		.pipe(zip('tab-rotate-' + manifest.version + '.zip'))
+		.pipe(gulp.dest('zip'));
+});
+
+gulp.task('default', ['clean', 'project', 'app', 'bower', 'zip']);
