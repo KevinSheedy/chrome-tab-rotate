6ca0b294c8fe95a88d8bf8335bd0d42861de0bf2
Kevin Sheedy <kevinsheedy@gmail.com>
2017-10-31 18:57:29 +0000

    import ga.js

---
+++ app/import-analytics.js
@@ -0,0 +1,12 @@
+console.log('import-analytics.js');
+
+var _AnalyticsCode = 'UA-109046965-1';
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
