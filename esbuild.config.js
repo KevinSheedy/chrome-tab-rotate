bf1d01f4b31357406395ea5a726c56f57177f1cd
Kevin Sheedy <kevinsheedy@gmail.com>
2021-07-31 15:46:18 +0100

    esbuild build file

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,12 +1,12 @@
-require('esbuild')
-  .build({
-    entryPoints: [
-      'src/background.js',
-      'src/settings.js',
-      'src/import-analytics.js',
-    ],
-    sourcemap: 'external',
-    bundle: true,
-    outdir: 'dist',
-  })
-  .catch(() => process.exit(1));
+let { build } = require('esbuild');
+
+build({
+  entryPoints: [
+    'src/background.js',
+    'src/settings.js',
+    'src/import-analytics.js',
+  ],
+  sourcemap: 'external',
+  bundle: true,
+  outdir: 'dist',
+}).catch(() => process.exit(1));
