e44c4bd9c28b997cf8e404a82c890ebc4c2f429a
Kevin Sheedy <kevinsheedy@gmail.com>
2021-07-31 07:37:34 +0100

    webpack spike

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/background.js',
   output: {
     filename: 'background.bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'manifest.json', to: '' },
+        { from: 'README.md', to: '' },
+      ],
+    }),
+  ],
 };
