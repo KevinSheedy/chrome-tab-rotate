807aa9ecc4a7f6e26a31d6def04ba55b0e55f69f
Kevin Sheedy <kevinsheedy@gmail.com>
2021-07-31 07:54:51 +0100

    webpack spike

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/background.js',
+  entry: {
+    background: './src/background.js',
+    settings: './src/settings.js',
+    'import-analytics': './src/import-analytics.js',
+  },
   output: {
-    filename: 'background.bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
@@ -14,6 +19,9 @@ module.exports = {
         { from: 'manifest.json', to: '' },
         { from: 'README.md', to: '' },
       ],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
     }),
   ],
 };
